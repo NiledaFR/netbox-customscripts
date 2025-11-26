799494b6bcf28877c63b852deee8c4ef1af7beb7
NiledaFR <adelin@nileda.fr>
2025-11-26 15:37:44 +0100

    Update nouveau-site.py

--- nouveau-site.py
+++ nouveau-site.py
@@ -35,8 +35,7 @@ class NewSite(Script):
         choices=(('ROUTAGE','Routage'),('IPSEC','IPSec'),('L2','L2'))
     )
     
-    palo = Manufacturer.get(name="Palo Alto")
-    palo_models = DeviceType.filter(manufacturer=palo.id)
+    palo_models = DeviceType.objects.filter(manufacturer=264)
     region = ObjectVar(
         description="Quel type de site?",
         model=palo_models
