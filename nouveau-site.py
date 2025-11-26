f263a1f0bf6c0cca104826d0dd00acf3d884fbf7
NiledaFR <adelin@nileda.fr>
2025-11-26 22:42:26 +0100

    Update nouveau-site.py

--- nouveau-site.py
+++ nouveau-site.py
@@ -94,7 +94,7 @@ class NewSite(Script):
         # Create L3 Equipement
         l3_role = DeviceRole.objects.get(name='Firewall')
 
-        if data['model_niveau3'] != ""
+        if data['model_niveau3'] != "":
             firewall = Device(
                 device_type=data['model_niveau3'],
                 name=data['code_site']+"-FW01",
