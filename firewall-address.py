385b6d24a6916d6eac75481e8310e42606bdd02f
NiledaFR <adelin@nileda.fr>
2025-11-26 10:29:26 +0100

    Update firewall-address.py

--- firewall-address.py
+++ firewall-address.py
@@ -6,16 +6,16 @@ from ipam.models import IPAddress, Prefix
 
 
 class LinkFirewallToAddress(Script):
-    class Meta:
-        name = "Liaison Firewall-Adresses"
-        description = "Permet de lier les Firewall à leurs adresses"
+	class Meta:
+		name = "Liaison Firewall-Adresses"
+		description = "Permet de lier les Firewall à leurs adresses"
 
-    def run(self, data, commit):
-        devicerole=DeviceRole.objects.filter(name="Firewall")[0]
-        firewalls=Device.objects.filter(role=devicerole.id)
-        for firewall in firewalls:
-            if firewall.name == "ANA-FW01":
-                site=firewall.site
-                prefixes=Prefix.objects.filter(site=site)
-				for prefix in prefixes
-						self.log_success(f"prefix found for site {site}: {prefix.prefix}")
+	def run(self, data, commit):
+		devicerole=DeviceRole.objects.filter(name="Firewall")[0]
+		firewalls=Device.objects.filter(role=devicerole.id)
+		for firewall in firewalls:
+			if firewall.name == "ANA-FW01":
+				site=firewall.site
+				prefixes=Prefix.objects.filter(site=site)
+				for prefix in prefixes:
+					self.log_success(f"prefix found for site {site}: {prefix.prefix}")
