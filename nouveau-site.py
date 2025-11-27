996a365815ff0ad58f57e67bddd06953ac5e4ebc
NiledaFR <adelin@nileda.fr>
2025-11-27 09:55:36 +0100

    Update nouveau-site.py

--- nouveau-site.py
+++ nouveau-site.py
@@ -7,7 +7,7 @@ from extras.choices import CustomFieldTypeChoices
 from ipam.models import VLAN, Prefix, Role, IPAddress
 from core.models import ObjectType
 
-from niledaUtils.tools import AvailablePrefixes
+from .niledaUtils.tools import AvailablePrefixes
 
 
 class NewSite(Script):
