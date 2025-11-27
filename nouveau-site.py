d67608095734c476ce9b803569123fd37e1c35ba
NiledaFR <adelin@nileda.fr>
2025-11-27 08:50:52 +0100

    Update nouveau-site.py

--- nouveau-site.py
+++ nouveau-site.py
@@ -15,42 +15,35 @@ class NewSite(Script):
         description = "Permet de créer un nouveau site"
 
     code_site = StringVar(
-        description="Code du nouveau site",
-        required=False
+        description="Code du nouveau site"
     )
 
     nom_du_site = StringVar(
-        description="Nom du nouveau site",
-        required=False
+        description="Nom du nouveau site"
     )
 
     affectation_du_site = ObjectVar(
         description="Quel type de site?",
-        model=Region,
-        required=False
+        model=Region
     )
     
     adresse_postale = StringVar(
-        description="Adresse postale du nouveau site",
-        required=False
+        description="Adresse postale du nouveau site"
     )
 
     type_de_site = ChoiceVar(
         description="Type de site",
-        choices=(('OUI','Intermédiaire (avec backbone)'),('NON','Final (sans backbone)')),
-        required=False
+        choices=(('OUI','Intermédiaire (avec backbone)'),('NON','Final (sans backbone)'))
     )
 
     type_d_interco = ChoiceVar(
         description="Type d'interco",
-        choices=(('ROUTAGE','Routage'),('IPSEC','IPSec'),('L2','L2')),
-        required=False
+        choices=(('ROUTAGE','Routage'),('IPSEC','IPSec'),('L2','L2'))
     )
 
     fabricant_niveau3 = ObjectVar(
         description="Fabricant du device de niveau 3",
-        model=Manufacturer,
-        required=False
+        model=Manufacturer
     )
     
     model_niveau3 = ObjectVar(
@@ -58,8 +51,7 @@ class NewSite(Script):
         model=DeviceType,
         query_params={
             'manufacturer_id': '$fabricant_niveau3'
-        },
-        required=False
+        }
     )
 
     vlans_en_25 = MultiObjectVar(
@@ -76,7 +68,7 @@ class NewSite(Script):
 
     def run(self, data, commit):
 
-        # Create the new site
+        # Création de l'objet site avec les données entrées dans l'interface web
         site = Site(
             name=data['code_site']+" - "+data['nom_du_site'],
             slug=slugify(data['nom_du_site']),
@@ -87,12 +79,14 @@ class NewSite(Script):
         )
         site.full_clean()
         site.save()
-        typedesite=site.cf.get('TYPE_INTERCO')
-        self.log_success(f"Created new site: {site}, avec comme type {typedesite}")
 
-        # Create L3 Equipement
+        self.log_success(f"Created new site: {site}")
+
+        # On récupère le role de l'objet du niveau 3
+        # Sur un nouveau site, ça ne peut être qu'un FW
         l3_role = DeviceRole.objects.get(name='Firewall')
 
+        # Création de l'objet firewall avec les données entrées dans l'interface web
         firewall = Device(
             device_type=data['model_niveau3'],
             name=data['code_site']+"-FW01",
@@ -104,7 +98,9 @@ class NewSite(Script):
         firewall.save()
         self.log_success(f"Created firewall: {firewall}")
 
-        # Create Prefixes
+        ## Création des préfixes pour chaque VLAN
+
+        # Récupération des préfixes dispo sur les plages réservée en /25
         Prefix25Reserved=Prefix.objects.filter(role=Role.objects.get(name="Sites Distants - Infra Cible - 25").id)
         list25AvailablePrefixes = []
         for prefix in Prefix25Reserved:
@@ -113,8 +109,16 @@ class NewSite(Script):
                 for availablePrefix in list(availablePrefixList.subnet(25)):
                     list25AvailablePrefixes.append(availablePrefix)
 
+        # On va utilisé le tableau des préfixes dispos, pour ne pas le regénérer tout le temps, 
+        # on utilise une variable qui indiquera l'entrée du tableau utilisée
+        # cette variable est incrémentée à chaque VLAN
         nb_prefix=0
+
+        # On parcours les VLANs désigné dans l'interface web puis on créé un préfix pour chacun
+        # On créé également une adresse IP pour le FW et on l'affecte à une de ses interfaces suivant le cas (PUB, PRIV ou MGMT)
         for vlan in data['vlans_en_25']:
+
+            # Création du préfixe
             vlanNameArray=vlan.name.lower().split('-',1)
             codeSite=data['code_site'].lower()
             prefix=Prefix(
@@ -127,86 +131,125 @@ class NewSite(Script):
             )
             prefix.full_clean()
             prefix.save()
+
+            # Récupération de la dernière IP du préfixe pour affectation au palo
             lastIpInPrefix=(prefix.prefix.broadcast-1).format()+"/"+str(prefix.mask_length)
 
+            # Création de cette dernière IP
             lastIpInPrefix=IPAddress(address=lastIpInPrefix, status="active", dns_name=firewall.name, description=firewall.device_type.model)
             lastIpInPrefix.full_clean()
             lastIpInPrefix.save()
 
             if "PRIV-" in prefix.vlan.group.name:
+                # Si on est dans le VLAN PRIV-MGMT, on créé aussi l'avant dernière IP pour le OOB du Palo
+                # Sinon on affecte l'adresse IP à ethernet1/7 pour le PRIV
                 if prefix.vlan.group.name == "PRIV-MGMT":
+                    # Création de l'avant dernière IP
                     preLastIpInPrefix=(prefix.prefix.broadcast-2).format()+"/"+str(prefix.mask_length)
                     preLastIpInPrefix=IPAddress(address=preLastIpInPrefix, status="active", dns_name=firewall.name+"-oob", description=firewall.device_type.model)
                     preLastIpInPrefix.full_clean()
                     preLastIpInPrefix.save()
                     preLastIpInPrefix.snapshot()
                     firewall.snapshot()
+                    # Affectation de l'avant-dernière IP au port management du Firewall et définition de celle-ci comme OOB
                     interface=firewall.interfaces.get(name="management")
                     preLastIpInPrefix.assigned_object = interface
                     preLastIpInPrefix.save()
                     firewall.oob_ip_id = preLastIpInPrefix.id
                     firewall.save()
+
+                    # Affectation de l'adresse IP au port 1/7 du palo
                     interface=firewall.interfaces.get(name="ethernet1/7")
                 else:
+                    # Si hors du management, on ne fait que l'affectation au port 1/3
                     interface=firewall.interfaces.get(name="ethernet1/3")
+            # Si le vlan fait parti du groupe PUB, c'est que l'IP est affectée à l'interface 1/5
             if "PUB-" in prefix.vlan.group.name:
                 interface=firewall.interfaces.get(name="ethernet1/5")
+
+            # Liaison de l'IP au firewall
             lastIpInPrefix.assigned_object = interface
             lastIpInPrefix.save()
 
+            # On log la création du préfix pour le VLAN
             self.log_success(f"Create prefix {list25AvailablePrefixes[nb_prefix]} for vlan {vlan}")
+
+            # On incrémente la variable de positionnement dans le tableau
             nb_prefix+=1
 
-        
-        # # Create routers
-        # router_role = DeviceRole.objects.get(name='WAN Router')
-        # for i in range(1, data['router_count'] + 1):
-        #     router = Device(
-        #         device_type=data['router_model'],
-        #         name=f'{site.slug.upper()}-RTR-{i}',
-        #         site=site,
-        #         status=DeviceStatusChoices.STATUS_PLANNED,
-        #         role=router_role
-        #     )
-        #     router.save()
-        #     self.log_success(f"Created new router: {router}")
+        # Récupération des préfixes dispo sur les plages réservée en /23
+        Prefix23Reserved=Prefix.objects.filter(role=Role.objects.get(name="Sites Distants - Infra Cible - 23").id)
+        list23AvailablePrefixes = []
+        for prefix in Prefix23Reserved:
+            availablePrefixes = prefix.get_available_prefixes()
+            for availablePrefixList in availablePrefixes.iter_cidrs():
+                for availablePrefix in list(availablePrefixList.subnet(23)):
+                    list23AvailablePrefixes.append(availablePrefix)
 
-        # # Create APs
-        # ap_role = DeviceRole.objects.get(name='Wireless AP')
-        # for i in range(1, data['ap_count'] + 1):
-        #     ap = Device(
-        #         device_type=data['ap_model'],
-        #         name=f'{site.slug.upper()}-AP-{i}',
-        #         site=site,
-        #         status=DeviceStatusChoices.STATUS_PLANNED,
-        #         role=ap_role
-        #     )
-        #     ap.save()
-        #     self.log_success(f"Created new AP: {router}")
-        
-        # # Create Servers
-        # server_role = DeviceRole.objects.get(name='vSphere')
-        # for i in range(1, data['server_count'] + 1):
-        #     server = Device(
-        #         device_type=data['server_model'],
-        #         name=f'{site.slug.upper()}-VSP-{i}',
-        #         site=site,
-        #         status=DeviceStatusChoices.STATUS_PLANNED,
-        #         role=server_role
-        #     )
-        #     server.save()
-        #     self.log_success(f"Created new server: {router}")
+        # On va utilisé le tableau des préfixes dispos, pour ne pas le regénérer tout le temps, 
+        # on utilise une variable qui indiquera l'entrée du tableau utilisée
+        # cette variable est incrémentée à chaque VLAN
+        nb_prefix=0
 
-        # # Generate a CSV table of new devices
-        # output = [
-        #     'name,make,model'
-        # ]
-        # for device in Device.objects.filter(site=site):
-        #     attrs = [
-        #         device.name,
-        #         device.device_type.manufacturer.name,
-        #         device.device_type.model
-        #     ]
-        #     output.append(','.join(attrs))
+        # On parcours les VLANs désigné dans l'interface web puis on créé un préfix pour chacun
+        # On créé également une adresse IP pour le FW et on l'affecte à une de ses interfaces suivant le cas (PUB, PRIV ou MGMT)
+        for vlan in data['vlans_en_23']:
 
-        # return '\n'.join(output)
+            # Création du préfixe
+            vlanNameArray=vlan.name.lower().split('-',1)
+            codeSite=data['code_site'].lower()
+            prefix=Prefix(
+                scope_id=site.id,
+                prefix=list23AvailablePrefixes[len(list23AvailablePrefixes)-1-nb_prefix],
+                status='active',
+                vlan=vlan,
+                scope_type=ObjectType.objects.get(app_label='dcim',model='site'),
+                description='lan_'+vlanNameArray[0]+'_'+codeSite+'-'+vlanNameArray[1]
+            )
+            prefix.full_clean()
+            prefix.save()
+
+            # Récupération de la dernière IP du préfixe pour affectation au palo
+            lastIpInPrefix=(prefix.prefix.broadcast-1).format()+"/"+str(prefix.mask_length)
+
+            # Création de cette dernière IP
+            lastIpInPrefix=IPAddress(address=lastIpInPrefix, status="active", dns_name=firewall.name, description=firewall.device_type.model)
+            lastIpInPrefix.full_clean()
+            lastIpInPrefix.save()
+
+            if "PRIV-" in prefix.vlan.group.name:
+                # Si on est dans le VLAN PRIV-MGMT, on créé aussi l'avant dernière IP pour le OOB du Palo
+                # Sinon on affecte l'adresse IP à ethernet1/7 pour le PRIV
+                if prefix.vlan.group.name == "PRIV-MGMT":
+                    # Création de l'avant dernière IP
+                    preLastIpInPrefix=(prefix.prefix.broadcast-2).format()+"/"+str(prefix.mask_length)
+                    preLastIpInPrefix=IPAddress(address=preLastIpInPrefix, status="active", dns_name=firewall.name+"-oob", description=firewall.device_type.model)
+                    preLastIpInPrefix.full_clean()
+                    preLastIpInPrefix.save()
+                    preLastIpInPrefix.snapshot()
+                    firewall.snapshot()
+                    # Affectation de l'avant-dernière IP au port management du Firewall et définition de celle-ci comme OOB
+                    interface=firewall.interfaces.get(name="management")
+                    preLastIpInPrefix.assigned_object = interface
+                    preLastIpInPrefix.save()
+                    firewall.oob_ip_id = preLastIpInPrefix.id
+                    firewall.save()
+
+                    # Affectation de l'adresse IP au port 1/7 du palo
+                    interface=firewall.interfaces.get(name="ethernet1/7")
+                else:
+                    # Si hors du management, on ne fait que l'affectation au port 1/3
+                    interface=firewall.interfaces.get(name="ethernet1/3")
+            # Si le vlan fait parti du groupe PUB, c'est que l'IP est affectée à l'interface 1/5
+            if "PUB-" in prefix.vlan.group.name:
+                interface=firewall.interfaces.get(name="ethernet1/5")
+
+            # Liaison de l'IP au firewall
+            lastIpInPrefix.assigned_object = interface
+            lastIpInPrefix.save()
+
+            # On log la création du préfix pour le VLAN
+            self.log_success(f"Create prefix {list23AvailablePrefixes[len(list23AvailablePrefixes)-1-nb_prefix]} for vlan {vlan}")
+
+            # On incrémente la variable de positionnement dans le tableau
+            nb_prefix+=1
